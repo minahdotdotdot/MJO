4cf0611140b50630bca1e69e30174365d2ca944c
Minah <minah@github.com>
2018-10-08 18:36:47 -0600

    nonempty

---
+++ time_step.jl
@@ -0,0 +1,18 @@
+function f_euler(initial_state:: MJO_State, params::MJO_params, h::Float64, N::Int, every::Int)
+    tend = deepcopy(initial_state)
+    evol = Array{MJO_State,1}(undef,div(N, every)+1)
+    evol[1] = initial_state
+    state = deepcopy(initial_state)
+    for i = 2 : N+1
+        dxdt(params, state, tend);
+        if istherenan(tend) == true || isthereinf(tend) ==true
+            return evol[1:div(i, every)]
+        end
+        state = state + h * tend;
+        if rem(i,every) == 1
+            evol[1+div(i, every)] = state
+        end
+        
+    end
+    return evol
+end
