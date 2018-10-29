0ebca71a0c9a325bb3468d9d52111fed8f8ed512
Minah <minah@github.com>
2018-10-29 17:49:52 -0600

    tweak fig size

--- mjo_sim_pyplot.jl
+++ mjo_sim_pyplot.jl
@@ -17,8 +17,8 @@ function savecontourmaps(evol::Array{MJO_State,1}, str::String; draw::Symbol=:co
         minval = minimum(evolfield);
         maxval = maximum(evolfield);
         for j = 1 : length(evolfield)
-            fig, ax = subplots(); 
-            fig[:set_size_inches](13,2); 
+            fig, ax = subplots(fig_size(13,2)); 
+            #fig[:set_size_inches](13,2); 
             ax[:set_aspect]("equal");
             fig[:colorbar](
                 ax[draw](
