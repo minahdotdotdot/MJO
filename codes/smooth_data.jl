023ec3d63127de8151198bc286e3aa6b60f173a0
Minah <minah@github.com>
2019-03-13 19:59:49 -0600

    allow inputting ranges

--- codes/smooth_data.jl
+++ codes/smooth_data.jl
@@ -121,15 +121,15 @@ function smoother(q::Array{T,2};
 end
 
 using GaussianRandomFields, PyPlot
-function genRandField()
+function genRandField(;x=1:1:162, y=1:1:1440)
     #mat = Matern(0.5,2.0)
     #covar = CovarianceFunction(2,mat)
     grf = GaussianRandomField(
         CovarianceFunction(2,Matern(0.5,2.0)),
         CirculantEmbedding(),
-        1:1:162, 
-        1:1:1440,
+        x, 
+        y,
         minpadding=250
         )
-    return sample(grf)
+    return grf
 end
