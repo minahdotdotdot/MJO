98437cfe65bedcc2f456efdc7dd6ebf85a08cf28
Minah <minah@github.com>
2019-04-26 11:32:50 -0600

    added correct H to moisture divflux

--- codes/runsim.jl
+++ codes/runsim.jl
@@ -1,6 +1,7 @@
 include("time_step3.jl")
 h_time = 0.0045 #15 min 0.0009; #3 min  
-every = 48 #12 hours 2.4 hrs
-N = every*365*2 #a day.#every#*365*2;  #2 years
-H1 = range(1.8, stop=1.2, length=9);
-imex_print(N, every, h_time, "15min-12hrH",H1=H1[01])                                                   
+every = 96 * 7; # 1 week
+N = every * 52 * 4 #4years
+H1 = 0.03;
+NA=range(0.03, stop=0.2, length=9)
+imex_print(N, every, h_time, "15min-12hrH",H1=H1, NA=NA[1]) 
